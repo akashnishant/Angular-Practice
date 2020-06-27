be094d5f80ff070ae00fcd03a8fa757cc60390e1
Akash Nishant <akashnishant25@gmail.com>
2020-06-28 01:06:24 +0530

    created an employee module
    Command: ng g module employee
    
    1. employee module gets created inside the src folder with a employee.module.ts file
    
    2. Used the EmployeeModule in the imports section of app.module.ts
    
    3. Imported the EmployeeModule in the app.module.ts

--- angular-practice/src/app/app.module.ts
+++ angular-practice/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { EmployeeModule } from './employee/employee.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
 
@@ -12,7 +13,8 @@ import { TestComponent } from './test/test.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    EmployeeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
